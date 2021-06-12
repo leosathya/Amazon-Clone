045c0ea008ac09e775f020eeeb7544d1ddc0c333
sathya <leosathya10@gmail.com>
2021-06-12 13:15:24 +0530

    Checkout page complete with both adding and removing product functionality

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from './Subtotal'
+import CheckoutProducts from './CheckoutProducts'
+import { useStateValue } from '../StateProvider'
 
 function Checkout() {
+    const[{basket, dispatch}] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout_left">
                 <img className="checkout_ad" src="/images/bannerad.png"/>
                 <div>
                     <h2 className="checkout_title">Your Shopping Basket </h2>
+
+                    {basket.map(item => (
+                        <CheckoutProducts
+                           id={item.id}
+                           title={item.title}
+                           img={item.img}
+                           price={item.price}
+                           rating={item.rating}
+                        />
+                    ))}
                 </div>
             </div>
             <div className="checkout_right">
