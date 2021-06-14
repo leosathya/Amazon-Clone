86a6a3400b87c7b7db7304a6e12c3890f90bd9a0
sathya <leosathya10@gmail.com>
2021-06-14 09:12:31 +0530

    Hosted on Firebase

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,13 +5,14 @@ import CheckoutProducts from './CheckoutProducts'
 import { useStateValue } from '../StateProvider'
 
 function Checkout() {
-    const[{basket, dispatch}] = useStateValue();
+    const[{basket, user}, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
             <div className="checkout_left">
                 <img className="checkout_ad" src="/images/bannerad.png"/>
                 <div>
+                    <h3>Hello {user?.email}</h3>
                     <h2 className="checkout_title">Your Shopping Basket </h2>
 
                     {basket.map(item => (
